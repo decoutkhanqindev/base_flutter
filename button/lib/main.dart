7352ba205941c96766616929fa8edde8096cdc8b
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-06-27 23:08:35 +0700

    DropdownButton and DropdownButtonFormField

--- button/lib/main.dart
+++ button/lib/main.dart
@@ -2,14 +2,52 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: SafeArea(child: Scaffold(body: Body())),
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(child: Content()),
     ),
   );
 }
 
-class Body extends StatelessWidget {
-  const Body({super.key});
+class Content extends StatefulWidget {
+  const Content({super.key});
+
+  @override
+  State<Content> createState() => _ContentState();
+}
+
+class _ContentState extends State<Content> {
+  List<String> items = ["Male", "Female"];
+  late String selectedItem;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedItem = items[0];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          DropdownButtonDemo(
+            items: items,
+            onChanged: (item) {
+              setState(() {
+                selectedItem = item;
+              });
+            },
+          ),
+          Text(selectedItem),
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonDemo extends StatelessWidget {
+  const ButtonDemo({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -33,8 +71,43 @@ class Body extends StatelessWidget {
                   fixedSize: Size(200, 200),
                   side: BorderSide(width: 4, color: Colors.red),
                 ),
-            child: Text("Button"),
+            child: const Text("Button"),
           ),
+    );
+  }
+}
+
+class DropdownButtonDemo extends StatefulWidget {
+  final List<String> items;
+  final ValueChanged<String> onChanged;
+
+  const DropdownButtonDemo({
+    super.key,
+    required this.items,
+    required this.onChanged,
+  });
+
+  @override
+  State<DropdownButtonDemo> createState() => _DropdownButtonDemoState();
+}
+
+class _DropdownButtonDemoState extends State<DropdownButtonDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const Text("SEX"),
+        // DropdownButtonFormField
+        DropdownButton(
+          borderRadius: BorderRadius.circular(10),
+          items: widget.items
+              .map((item) => DropdownMenuItem(value: item, child: Text(item)))
+              .toList(),
+          onChanged: (item) {
+            widget.onChanged(item.toString());
+          },
+        ),
+      ],
     );
   }
 }
